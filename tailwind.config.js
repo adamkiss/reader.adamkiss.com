c8411d538c7a0cb2540d1217ca86e81d34965474
Adam Kiss <iam@adamkiss.com>
2020-07-26 22:12:30 +0200

    Fix build WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,17 @@
-const sansFamilies = [
-	'-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'
-]
-const fontWithSans = font => {
-	return [font, ...sansFamilies]
-}
-
 /*
 ** TailwindCSS Configuration File
 **
 ** Docs: https://tailwindcss.com/docs/configuration
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
+const defaultConfig = require('tailwindcss/defaultConfig')
+
 module.exports = {
 	theme: {
 		fontFamily: {
-			sans: [...sansFamilies],
-			text: ['alegreya', ...sansFamilies],
-			headline: ['alegreya-sc', ...sansFamilies]
+			sans: [...defaultConfig.theme.fontFamily.sans],
+			text: ['alegreya', ...defaultConfig.theme.fontFamily.sans],
+			headline: ['alegreya-sc', ...defaultConfig.theme.fontFamily.sans]
 		},
 		extend: {
 			screens: {
@@ -62,11 +57,8 @@ module.exports = {
 		// Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
 		enabled: process.env.NODE_ENV === 'production',
 		content: [
-			'components/**/*.vue',
-			'layouts/**/*.vue',
-			'pages/**/*.vue',
-			'plugins/**/*.js',
-			'nuxt.config.js'
+			'templates/**/*.njk',
+			'site/**/*.html'
 		]
 	},
 	corePlugins: {
