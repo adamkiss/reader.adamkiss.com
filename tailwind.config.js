198101a02be4773388ab1cb66c345d5cba16c411
Adam Kiss <iam@adamkiss.com>
2020-08-01 19:13:02 +0200

    Purge fix - the correct files to purge with

--- tailwind.config.js
+++ tailwind.config.js
@@ -58,8 +58,8 @@ module.exports = {
 		// Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
 		enabled: process.env.NODE_ENV === 'production',
 		content: [
-			'src/**/*.js',
-			'src/**/*.svelte',
+			'site/*.html',
+			'functions-templates/**/*.njk',
 		]
 	},
 	corePlugins: {
