e3d651fe2401a0f66fde2cb38695e59e2ba2e022
Adam Kiss <iam@adamkiss.com>
2020-08-06 20:16:38 +0200

    Hotfix ao3

--- functions/ao3/ao3.js
+++ functions/ao3/ao3.js
@@ -4,7 +4,7 @@ const layout = require('../shared/layout')
 
 exports.handler = async event => {
 	try {
-		const [, , story, chapter] = event.path.split('/')/push(null)
+		const [, , story, chapter] = event.path.split('/').push(null)
 		const parsed = await parse({story, chapter})
 		const title = parsed.title + (parsed.oneshot ? '' : `, ${parsed.currentChapter.name}`)
 		return { statusCode: 200, body: layout(title, template(parsed)) }
